bbe7cd2bdcb111a08e7c4963e7f9210e78b35f12
Allen Zheng <strburst@users.noreply.github.com>
2016-07-06 18:07:02 -0700

    Fix adding of tetris keybindings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -131,9 +131,11 @@
 (global-undo-tree-mode)
 
 ;; Ergonomic tetris is important
-(define-key tetris-mode-map (kbd "h") 'tetris-move-left)
-(define-key tetris-mode-map (kbd "j") 'tetris-rotate-next)
-(define-key tetris-mode-map (kbd "k") 'tetris-rotate-prev)
-(define-key tetris-mode-map (kbd "l") 'tetris-move-right)
+(add-hook 'tetris-mode-hook
+	  (lambda ()
+	     (define-key tetris-mode-map (kbd "h") 'tetris-move-left)
+	     (define-key tetris-mode-map (kbd "j") 'tetris-rotate-next)
+	     (define-key tetris-mode-map (kbd "k") 'tetris-rotate-prev)
+	     (define-key tetris-mode-map (kbd "l") 'tetris-move-right)))
 
 (setq init-complete t)
