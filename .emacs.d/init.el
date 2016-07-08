1df257ee3405963fc49f7a968bc0c49f7a4bb409
Allen Zheng <strburst@users.noreply.github.com>
2016-07-08 10:00:40 -0700

    Add the magic of nyan cat to Emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -127,6 +127,21 @@
 (require 'flycheck)
 (require 'magit)
 
+(require 'nyan-mode)
+(nyan-mode 1)
+
+(defun toggle-max-nyan ()
+  "Toggle the music and animation in nyan mode."
+  (interactive)
+  (if nyan-animate-nyancat
+      (progn
+	(nyan-stop-animation)
+	(nyan-stop-music))
+      (nyan-start-animation)
+      (nyan-start-music)))
+
+(global-set-key (kbd "<f12>") 'toggle-max-nyan)
+
 (require 'undo-tree)
 (global-undo-tree-mode)
 
