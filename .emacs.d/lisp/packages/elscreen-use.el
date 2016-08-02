dbe60e86b55881744a680b9edb995819d1317841
Allen Zheng <strburst@users.noreply.github.com>
2016-08-01 17:53:05 -0700

    Add basic elscreen configuration

---
+++ .emacs.d/lisp/packages/elscreen-use.el
@@ -0,0 +1,23 @@
+;;; elscreen-use.el --- Add tabs to Emacs, similar to GNU Screen
+
+(use-package elscreen
+  :init
+  ;; Change prefix key to C-f
+  (setq elscreen-prefix-key (kbd "C-f"))
+  (define-key evil-motion-state-map (kbd "C-f") nil)
+  (define-key global-map (kbd "C-f") nil)
+
+  :config
+  (elscreen-start)
+  (setq elscreen-tab-display-control nil)  ; Don't display dummy control tab
+
+  (defun elscreen-toggle-tabs-full (&optional arg)
+    (interactive)
+    (elscreen-toggle-display-tab)
+    (elscreen-toggle-display-screen-number))
+
+  (elscreen-toggle-display-tab))
+
+(provide 'elscreen-use)
+
+;;; elscreen-use.el ends here
