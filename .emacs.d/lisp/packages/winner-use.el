b6bea69b692c9ebaf7cc3af3779bf9e9dc97a453
Allen Zheng <strburst@users.noreply.github.com>
2016-08-04 14:03:32 -0700

    Add some keybindings for winner-mode

--- .emacs.d/lisp/packages/winner-use.el
+++ .emacs.d/lisp/packages/winner-use.el
@@ -1,8 +1,15 @@
 ;;; winner-use.el --- Revert to previous window states (bundled with Emacs)
 
 (use-package winner
+  :bind
+  (:map winner-mode-map
+        ("C-w C-h" . winner-undo)
+        ("C-w C-l" . winner-redo))
+
   :config
-  (winner-mode 1))
+  (winner-mode 1)
+
+  :demand)
 
 (provide 'winner-use)
 
