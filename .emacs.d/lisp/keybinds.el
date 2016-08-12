c7103b07ddd7879d21c5cb20370661f7f96a3c6f
Allen Zheng <strburst@users.noreply.github.com>
2016-08-11 18:08:22 -0700

    Make leader-map not a macro; fix misnamed function

--- .emacs.d/lisp/keybinds.el
+++ .emacs.d/lisp/keybinds.el
@@ -5,22 +5,23 @@
       fallback-leader "C-c C-d"  ; Fallback used when evil is inactive
       fallback-leader-double "C-c C-d C-d")
 
-(defmacro leader-map (&rest maps)
+(defun leader-map (&rest maps)
   "Assign the given maps with prefix leader in evil normal mode and
 fallback-leader elsewhere."
-  `(progn
-     (general-define-key :keymaps 'evil-normal-state-map
-                         :prefix leader
-                         ,@maps)
-     (general-define-key :prefix fallback-leader
-                         ,@maps)))
+  (apply 'general-define-key
+         :keymaps 'evil-normal-state-map
+         :prefix leader
+         maps)
+  (apply 'general-define-key
+         :prefix fallback-leader
+         maps))
 
 (leader-map "f" 'find-file
             "d" 'switch-to-buffer)
 
 (global-set-key (kbd "<f2>") 'base-lib/echo-major-mode)
 (global-set-key (kbd "<f9>") 'base-lib/open-emacs-config)
-(global-set-key (kbd "<f12>") 'base-lib/list-packages)
+(global-set-key (kbd "<f12>") list-packages)
 
 (provide 'keybinds)
 
