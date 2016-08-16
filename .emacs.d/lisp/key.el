bffa1833630424b464c5f212a5181c20838d7f78
Allen Zheng <strburst@users.noreply.github.com>
2016-08-16 19:54:06 -0400

    Continue reworking key.el and bindings

--- .emacs.d/lisp/key.el
+++ .emacs.d/lisp/key.el
@@ -4,36 +4,40 @@
 
 (defvar key/leader "SPC")
 (defvar key/leader-double "SPC SPC")
-(defvar key/fallback-leader "M-SPC") ; Fallback used when evil is inactive
+(defvar key/fallback-leader "M-SPC")  ; Fallback used when evil is inactive
 (defvar key/fallback-leader-double "M-SPC SPC")
 
 (setq general-default-states '(normal insert emacs))
 
 (defun key/leader-map (&rest maps)
-  "Assign the given MAPS with prefix leader in evil normal mode and fallback-leader elsewhere."
+  "Assign the given MAPS with my preferred prefixes.
+
+These are `key/leader' in evil normal mode and `key/fallback-leader' elsewhere."
   (apply 'general-define-key
          :prefix key/leader
          :non-normal-prefix key/fallback-leader
          maps))
 
 (defun key/leader-double-map (&rest maps)
-  "Assign the given MAPS with prefix leader in evil normal mode and fallback-leader elsewhere."
+  "Assign the given MAPS with doubled variants of my preferred prefixes.
+
+These are `key/leader-double' in evil normal mode and
+`key/fallback-leader-double' elsewhere."
   (apply 'general-define-key
          :prefix key/leader-double
          :non-normal-prefix key/fallback-leader-double
          maps))
 
-;; Miscellaneous
-(key/leader-map "d" 'switch-to-buffer)
-
-;; File operations
+;; Files/buffers
 (key/leader-map "f d" 'vc-delete-file
                 "f f" 'find-file
+                "f j" 'switch-to-buffer
                 "f k" 'kill-this-buffer
+                "f q" 'save-buffers-kill-terminal
                 "f r" 'vc-rename-file
                 "f s" 'save-buffer)
 
-;; Emacs-Lisp-related
+;; Emacs Lisp
 (key/leader-map "l e" 'eval-last-sexp
                 "l i" 'eval-print-last-sexp)
 
