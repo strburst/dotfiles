12ba62f1eca1e14931caae14c3b81f6206be5c7c
Allen Zheng <strburst@users.noreply.github.com>
2016-09-01 20:18:38 -0400

    Add headers to custom; whitelist sml light theme

--- .emacs.d/custom.el
+++ .emacs.d/custom.el
@@ -1,8 +1,15 @@
+;;; custom.el --- Configuration by customize
+;;; Commentary:
+;;; Code:
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("a27c00821ccfd5a78b01e4f35dc056706dd9ede09a8b90c6955ae6a390eb1c1e" default)))
  '(package-selected-packages
    (quote
     (vimrc-mode magit ido-ubiquitous smex yasnippet elscreen xkcd nyan-mode evil-surround evil-org evil-numbers evil-commentary evil dash diminish use-package)))
@@ -13,3 +20,5 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+
+;;; custom.el ends here
