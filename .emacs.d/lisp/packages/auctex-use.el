f6478a7ab29e13c9310b097ee6d27f9922b4a32c
Allen Zheng <strburst@users.noreply.github.com>
2016-10-21 09:39:19 -0400

    Workaround bug in auctex and add compile on save

--- .emacs.d/lisp/packages/auctex-use.el
+++ .emacs.d/lisp/packages/auctex-use.el
@@ -4,7 +4,17 @@
 
 (require 'ensure)
 
+;; Workaround for auctex using signum instead of cl-signum
+(defalias 'signum 'cl-signum)
 (ensure/load-library 'auctex)
+
+;; Compile to preview whenever buffer is saved
+(add-hook 'LaTeX-mode-hook
+          (lambda ()
+            (add-hook 'after-save-hook
+                      (lambda ()
+                        (TeX-command-run-all nil))
+                      nil t)))
 
 (provide 'auctex-use)
 
